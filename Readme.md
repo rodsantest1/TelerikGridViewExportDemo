a95127bd4c55e3abce70a1ede968899685414771
Rodney Santiago <GD000007711@ups.com>
2022-01-04 18:21:22 -0500

    Add project files.

---
+++ Readme.md
@@ -0,0 +1,10 @@
+
+# Telerik RadGridView Exporting  
+
+8/7/21  
+
+ExportToXlsx [[Go]](https://docs.telerik.com/devtools/wpf/controls/radgridview/export/excel/export-xlsx)  
+
+Warning message when opening the Excel file [[Go]](https://docs.telerik.com/devtools/wpf/controls/radgridview/export/how-to/warning-message-excel)  
+
+Get the column of the corresponding cell [[Go]](https://docs.telerik.com/devtools/wpf/controls/radgridview/export/how-to/column-of-cell)  
---
+++ TelerikGridViewExportDemo/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Data;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Data;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders.OpenXml.Xlsx;
+using Telerik.Windows.Documents.Spreadsheet.FormatProviders.Pdf;
+using Telerik.Windows.Documents.Spreadsheet.Model;
+
+namespace TelerikGridViewExportDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            GridView1.ElementExportingToDocument += GridView1_ElementExportingToDocument;
+
+            GridView1.SelectionChanged += GridView1_SelectionChanged;
+            GridView1.SelectionChanging += GridView1_SelectionChanging;
+            GridView1.RowActivated += GridView1_RowActivated;
+        }
+
+        private void GridView1_RowActivated(object sender, Telerik.Windows.Controls.GridView.RowEventArgs e) { TestHandler(); }
+        private void GridView1_SelectionChanging(object sender, SelectionChangingEventArgs e) { TestHandler(); }
+        private void GridView1_SelectionChanged(object sender, SelectionChangeEventArgs e) { TestHandler(); }
+
+        private void TestHandler([CallerMemberName] string caller = "")
+        {
+            MessageBox.Show(caller);
+        }
+        private void UnselectButton_Click(object sender, RoutedEventArgs e)
+        {
+            GridView1.SelectedItem = null;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var content = "[" +
+                "{ \"break\":\"true\", \"lunch\":\"2:00:00\", \"temperatureC\":11,\"temperatureF\":51,\"summary\":\"Chilly\"}," +
+                "{ \"break\":\"false\", \"lunch\":\"5:00:00\", \"temperatureC\":20,\"temperatureF\":67,\"summary\":\"Scorching\"}," +
+                "{ \"break\":\"false\", \"lunch\":\"4:00:00\", \"temperatureC\":-4,\"temperatureF\":25,\"summary\":\"Freezing\"}," +
+                "{ \"break\":\"true\", \"lunch\":\"3:00:00\", \"temperatureC\":-10,\"temperatureF\":15,\"summary\":\"Warm\"}," +
+                "{ \"break\":\"true\", \"lunch\":\"1:00:00\", \"temperatureC\":-10,\"temperatureF\":15,\"summary\":\"Cool\"}]";
+            var data = JsonConvert.DeserializeObject<IEnumerable<WeatherForecast>>(content);
+
+            GridView1.ItemsSource = data;
+        }
+
+        //https://docs.telerik.com/devtools/wpf/controls/radgridview/export/how-to/column-of-cell
+        private void GridView1_ElementExportingToDocument(object sender, GridViewElementExportingToDocumentEventArgs e)
+        {
+            if (e.Element == ExportElement.Cell)
+            {
+                var cellExportingArgs = e as GridViewCellExportingEventArgs;
+                if (cellExportingArgs.Column == this.GridView1.Columns[2])
+                {
+                    (cellExportingArgs.VisualParameters as GridViewDocumentVisualExportParameters).Style = new Telerik.Windows.Controls.GridView.CellSelectionStyle() { IsBold = true };
+                }
+            }
+        }
+
+        private void ExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            var workbook = DesignWorkbook();
+
+            var xlsFormatProvider = new XlsxFormatProvider();
+            var pdfFormatProvider = new PdfFormatProvider();
+
+            var pdfFileName = @"C:\temp\test.pdf";
+            var xlsFileName = @"C:\temp\test.xlsx";
+
+            using (Stream output = new FileStream(xlsFileName, FileMode.Create))
+            {
+                xlsFormatProvider.Export(workbook, output);
+            }
+
+            using (Stream output = new FileStream(pdfFileName, FileMode.Create))
+            {
+                pdfFormatProvider.Export(workbook, output);
+            }
+
+            MessageBox.Show($"Exports completed normally on {DateTime.Now}");
+        }
+
+        private Workbook DesignWorkbook()
+        {
+            var workbook = GridView1.ExportToWorkbook();
+            // start
+            var worksheet = workbook.ActiveWorksheet;
+
+
+            worksheet.Rows[0].SetIsWrapped(true);
+
+            worksheet.Columns[0, 6].AutoFitWidth();
+
+            // finish
+            return workbook;
+        }
+    }
+
+    public class WeatherForecast
+    {
+        public string Break { get; set; }
+
+        public TimeSpan Lunch { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+
+    public class CustomFunction : AggregateFunction<WeatherForecast, double>
+    {
+        public CustomFunction()
+        {
+            this.AggregationExpression = items => Count(items);
+        }
+
+        private double Count(IEnumerable<WeatherForecast> source)
+        {
+            var itemCount = source.Count();
+            if (itemCount > 0)
+            {
+                var values = source.Where(i => i.Break.ToLower() == "true");
+
+                return values.Count();
+            }
+
+            return 0;
+        }
+    }
+
+    public class CustomTsAvgFunction : AggregateFunction<WeatherForecast, TimeSpan>
+    {
+        public CustomTsAvgFunction()
+        {
+            this.AggregationExpression = items => Average(items);
+        }
+
+        private TimeSpan Average(IEnumerable<WeatherForecast> source)
+        {
+            var intRes = source.Average(x => x.Lunch.TotalMinutes);
+            return TimeSpan.FromMinutes(intRes);
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BooleanToYesNo : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToBoolean(value) ? "Yes" : "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //return null;
+            throw new NotImplementedException();
+            //return FilterDescriptor.UnsetValue;
+
+        }
+    }
+}
